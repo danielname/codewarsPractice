390882bb7b7343b4cbade2bf5df768c7160311d7
Daniel Name <daniel.v.name@gmail.com>
2022-08-15 12:58:34 -0500

    combine strings, alphebetize, and show only unique characters problem

---
+++ sample-file-please-ignore.js
@@ -0,0 +1,15 @@
+let orange = "blue";
+function sampleTitle(orange){
+    return  orange === 5;
+}
+sampleTitle("cheese")
+
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+function longest(s1, s2) {
+    let str = s1 + s2;
+    let arr = str.split("");
+    arr.sort();
+    let arrStr = arr.join("");
+    return String.prototype.concat(...new Set(arrStr));
+}
