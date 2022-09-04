2e51b5721d35e9d1847c7b2d32da8271ddf3544e
Daniel Name <daniel.v.name@gmail.com>
2022-09-04 09:16:20 -0500

    factorial. preliminary solution, but it is rounding on large numbers that convert to scientific notation

--- js/codewars-practice.js
+++ js/codewars-practice.js
@@ -436,3 +436,23 @@ function postfixEvaluator(string) {
     }
     return parseInt(array.join(``));
 }
+
+
+// In mathematics, the factorial of integer n is written as n!. It is equal to the product of n and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+//
+// Your mission is simple: write a function that takes an integer n and returns the value of n!.
+//
+// You are guaranteed an integer argument. For any values outside the non-negative range, return null, nil or None (return an empty string "" in C and C++). For non-negative numbers a full length number is expected for example, return 25! =  "15511210043330985984000000"  as a string.
+function factorial(n){
+    if (n === 0) {
+        return `1`;
+    } else if (n > 0) {
+        let x = n;
+        let i = n - 1;
+        while (i > 1){
+            x*=i;
+            i--;
+        }
+        return x.toLocaleString('fullwide').split(`,`).join(``);
+    }
+}
