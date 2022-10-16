ff8ce0aa73a8ab97d21193143e42cf5e02f2dcb5
Daniel Name <daniel.v.name@gmail.com>
2022-10-15 22:45:21 -0500

    javascript warmup to keep my chops

--- js/codewars-practice.js
+++ js/codewars-practice.js
@@ -544,3 +544,18 @@ function dotCalculator (equation) {
         return
     }
         }
+
+//You have function one_two (oneTwo for Java) that returns 1 or 2 with 50% chance. one_two is already defined in a global scope and can be called everywhere.
+//
+// Your goal is to create function one_two_three (oneTwoThree for Java) that returns 1, 2 or 3 with equal probability using only one_two function.
+//
+// Do not try to cheat returning repeating non-random sequences. There is randomness test especially for this case.
+
+function one_two_three() {
+    let randInt = Math.floor(Math.random() * 3);
+    if (randInt === 1){
+        return 3;
+    } else{
+        return one_two();
+    }
+}
