bdd4d2d6238e6b25ad6e5ca4246279f17fb68c21
Daniel Name <daniel.v.name@gmail.com>
2022-10-30 15:24:15 -0500

    started time conversion problem

--- js/codewars-practice.js
+++ js/codewars-practice.js
@@ -579,3 +579,13 @@ function miniMaxSum(arr) {
     console.log (`${accumMin} ${accumMax}`)
 }
 
+//Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+//
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+function timeConversion(s) {
+    let time = s.slice(0,7);
+
+    return time;
+}
